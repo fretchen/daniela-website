f598f6bc38c8c28bdaf66353e97b8eb2d81d84e5
fretchen <polka.99gefiedert@icloud.com>
2026-03-26 15:33:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import vike from "vike/plugin";
 
 export default defineConfig({
+  base: "/daniela-website/",
   plugins: [vike(), react()],
   build: {
     outDir: "build",
